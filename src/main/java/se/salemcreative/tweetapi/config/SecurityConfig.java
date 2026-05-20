0ee9469f66442de32634d66ec572f11de37f14cd
Tobias Salem <tobias.salem@svk.se>
2026-05-20 09:16:29 +0200

    Upgrades to Spring Boot 3.5.14 [latest in the 3.5.x series as of today]. Upgrades to Java 25 LTS [long term support]

--- src/main/java/se/salemcreative/tweetapi/config/SecurityConfig.java
+++ src/main/java/se/salemcreative/tweetapi/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package se.salemcreative.tweetapi.config;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -12,6 +11,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
 import jakarta.servlet.http.HttpServletResponse;
 import se.salemcreative.tweetapi.security.JwtRequestFilter;
 
